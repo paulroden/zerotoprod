ca1251f9e3c26e859f69791353e89e3cab4988da
Paul Roden <git@paulroden.net>
2021-07-17 19:41:18 +0100

    use TcpListener for OS-assigned port

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use zerotoprod::run;
+use std::net::TcpListener;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    run()?.await
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind to random port");
+    run(listener)?.await
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,16 +1,18 @@
 //! tests/
 //!
 
+use std::net::TcpListener;
+
 #[actix_rt::test]
 async fn health_check_works() {
     // Arange
-    spawn_app();
+    let address = spawn_app();
     let client = reqwest::Client::new();
+    let target_uri = format!("{}/health_check", &address);
 
     // Act
-    let target_uri = "http://127.0.0.1:8000/health_check";
     let response = client
-        .get(target_uri)
+        .get(&target_uri)
         .send()
         .await
         .unwrap_or_else(|_| panic!("Failed to complete request to {}", target_uri));
@@ -20,10 +22,15 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length());
 }
 
-fn spawn_app() {
-    let server = zerotoprod::run().expect("Failed to bind address");
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind to random port");
+    // retrieve port assigned by the OS
+    let port = listener.local_addr().unwrap().port();
+    let server = zerotoprod::run(listener).expect("Failed to bind address");
     // Launch the server as a background task
     // tokio::spawn returns a handle to the spawned future,
     // but we have no use for it here, hence the non-binding let
     let _ = tokio::spawn(server);
+    // and return the application server address to the caller
+    format!("http://127.0.0.1:{}", port)
 }
