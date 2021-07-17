d2d2254d894d3aaa4b18d862c6853f2c96136492
Paul Roden <git@paulroden.net>
2021-07-18 00:33:00 +0100

    begin using config

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
 use zerotoprod::startup::run;
+use zerotoprod::configuration::get_configuration;
 use std::net::TcpListener;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind to random port");
+    // Try to read configuration and panic if this fails
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    // Take TCP port number from configuration
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
     run(listener)?.await
 }
