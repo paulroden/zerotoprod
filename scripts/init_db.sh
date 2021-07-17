97d71d864e72bee21d259945f9cd33a2c749f155
Paul Roden <git@paulroden.net>
2021-07-17 23:51:34 +0100

    add migration step and allow skip docker

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -25,15 +25,19 @@ DB_NAME="${POSTGRES_DB:=newsletter}"
 # Check if a custom port has been set, otherwise default to '5432'
 DB_PORT="${POSTGRES_PORT:=5432}"
 
-# Launch postgres via Docker
-docker run \
-    -e POSTGRES_USER=${DB_USER} \
-    -e POSTGRES_PASSWORD=${DB_PASSWORD} \
-    -e POSTGRES_DB=${DB_NAME} \
-    -p "${DB_PORT}":5432 \
-    -d postgres \
-    postgres -N 1000
-    #        ^^ increased maxium number of connections for testing purposes
+# Allow skipping of Docker launch if flag set (usually because a docker postgres image is already running)
+if [[ -z "${SKIP_DOCKER}" ]]
+then
+    # Launch postgres via Docker
+    docker run \
+        -e POSTGRES_USER=${DB_USER} \
+        -e POSTGRES_PASSWORD=${DB_PASSWORD} \
+        -e POSTGRES_DB=${DB_NAME} \
+        -p "${DB_PORT}":5432 \
+        -d postgres \
+        postgres -N 1000
+        #        ^^ increased maxium number of connections for testing purposes
+fi
 
 # Keep pinging Postgres until it's ready to accept commands
 export PGPASSWORD="${DB_PASSWORD}"
@@ -44,5 +48,10 @@ done
 
 >&2 echo "Postgres is up and running on port ${DB_PORT}!"
 
+>&2 echo "Postgres is up and running on port ${DB_PORT} - running migrations now!"
+
 export DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@localhost:${DB_PORT}/${DB_NAME}
 sqlx database create
+sqlx migrate run
+
+>&2 echo "Postgres has been migrated, ready to go!"
