629ae3dac46d0b2af408a5e67839e8d145eeefaf
Paul Roden <git@paulroden.net>
2021-07-18 14:24:38 +0100

    wrap with actix web logger middleware

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,12 @@ use zerotoprod::startup::run;
 use zerotoprod::configuration::get_configuration;
 use std::net::TcpListener;
 use sqlx::PgPool;
+use env_logger::Env;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    // Instantiate logging middleware
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
     // Try to read configuration and panic if this fails
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
--- src/startup.rs
+++ src/startup.rs
@@ -3,6 +3,7 @@
 use std::net::TcpListener;
 use actix_web::dev::{Server};
 use actix_web::{web, App, HttpServer};
+use actix_web::middleware::Logger;
 use sqlx::PgPool;
 use crate::routes::{health_check, subscribe};
 
@@ -15,6 +16,8 @@ pub fn run(
     // Capture `connection` from the surrounding environment
     let server = HttpServer::new(move || {
         App::new()
+            // Middleware for logging added using `wrap` method
+            .wrap(Logger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             .app_data(connection.clone())
