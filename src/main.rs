f2d7244f4be41ad62c5d91f0d23d8a6795fc94ea
Paul Roden <git@paulroden.net>
2021-07-25 14:41:52 +0100

    separate tracing subscription steps

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use zerotoprod::startup::run;
 use zerotoprod::configuration::get_configuration;
 use std::net::TcpListener;
 use sqlx::PgPool;
-use tracing::subscriber::set_global_default;
+use tracing::{Subscriber, subscriber::set_global_default};
 use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
 use tracing_log::LogTracer;
@@ -11,9 +11,26 @@ use tracing_log::LogTracer;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    // Redirect all `log`'s events to our tracing subscriber
-    LogTracer::init().expect("Failed to set logger.");
+    let subscriber = get_subscriber("zerotoprod".into(), "info".into());
+    init_subscriber(subscriber);
+    
+    // Try to read configuration and panic if this fails
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
+    // Take TCP port number from configuration
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
+    run(listener, connection_pool)?.await
+}
 
+
+/// Compose multiple layers into a `tracing` subscriber.
+pub fn get_subscriber(
+    name: String,
+    env_filter: String,
+) -> impl Subscriber + Send + Sync {
     // Fall back to having all tracing spans at INFO level or abive
     // if the RUST_LOG environment has not been set.
     let env_filter = EnvFilter::try_from_default_env()
@@ -25,19 +42,16 @@ async fn main() -> std::io::Result<()> {
     );
     // set-up tracing subscriber using settings above and
     // instantiate with `set_global_default`
-    let subscriber = Registry::default()
+    Registry::default()
         .with(env_filter)
         .with(JsonStorageLayer)
-        .with(formatting_layer);
+        .with(formatting_layer)
+}
+
+/// Register a subscriber as a global default to process span data.
+/// It should only be called once.
+pub fn init_subscriber(subscriber: impl Subscriber + Send + Sync) {
+    // Redirect all `log`'s events to our tracing subscriber
+    LogTracer::init().expect("Failed to set tracing logger");
     set_global_default(subscriber).expect("Failed to set tracing subscriber.");
-    
-    // Try to read configuration and panic if this fails
-    let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection_pool = PgPool::connect(&configuration.database.connection_string())
-        .await
-        .expect("Failed to connect to Postgres.");
-    // Take TCP port number from configuration
-    let address = format!("127.0.0.1:{}", configuration.application_port);
-    let listener = TcpListener::bind(address)?;
-    run(listener, connection_pool)?.await
 }
